c428a05f1f19eddf5184ecc8ca0937b757969ea7
yusuffomotoyosi <yusuffomotoyosi87@gmail.com>
2023-07-31 08:13:10 -0700

    23

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,13 +9,13 @@
  */
 int _strncmp(char *s1, char *s2, unsigned int b)
 {
-  unsigned int i;
-  for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
-    {
-      s1++;
-      s2++;
-    }
-  return (i - b);
+	unsigned int i;
+	for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
+	{
+		s1++;
+		s2++;
+	}
+	return (i - b);
 }
 /**
  * _strstr - locates a substring
@@ -25,17 +25,17 @@ int _strncmp(char *s1, char *s2, unsigned int b)
  */
 char *_strstr(char *haystack, char *needle)
 {
-  unsigned int len;
-  /*Get length of needle for strncmp*/
-  len = 0;
-  while (needle[len] != '\0')
-    len++;
-  /*compare substring*/
-  while (*haystack != '\0')
-    {
-      if (_strncmp(haystack, needle, len) == 0)
-	return (haystack);
-      haystack++;
-    }
-  return (NULL);
+	unsigned int len;
+	/*Get length of needle for strncmp*/
+	len = 0;
+	while (needle[len] != '\0')
+		len++;
+	/*compare substring*/
+	while (*haystack != '\0')
+	{
+		if (_strncmp(haystack, needle, len) == 0)
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
 }
