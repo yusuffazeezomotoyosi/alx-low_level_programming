1716133fe6a91c46b004212982e8853583d6188a
yusuffomotoyosi <yusuffomotoyosi87@gmail.com>
2023-08-04 04:33:46 -0700

    @#12

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: incoming argument
+ * @argv: incoming argument
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+
+  int total;
+
+  if (argc < 2)
+    {
+      printf("Error\n");
+      return (1);
+    }
+
+  total = (int) *argv[1] * (int) *argv[2];
+
+  printf("%d\n", total);
+
+  return (0);
+
+}
+
+#include <stdio.h>
+
+ /**
+  * main - Entry point
+  *
+  * @argc: incoming argument
+  * @argv: incoming argument
+  *
+  * Return: Always 0 (Success)
+  */
+int main(int argc, char **argv)
+{
+
+  int total;
+
+  if (argc < 2)
+    {
+      printf("Error\n");
+      return (1);
+    }
+
+  total = (int) *argv[1] * (int) *argv[2];
+
+  printf("%d\n", total);
+
+  return (0);
+
+}
